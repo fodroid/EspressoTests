f199db691c8cee0b290d684041a9cf14ad9fb53e
simplebam <yue.yang4@zhenai.com>
2018-09-05 17:09:49 +0800

    修改了相关的配置以及对testWebView方法进行了更深的描述

--- app/src/androidTest/java/me/shihao/instrumentedunittests/WebViewActivityTest.java
+++ app/src/androidTest/java/me/shihao/instrumentedunittests/WebViewActivityTest.java
@@ -1,7 +1,6 @@
 package me.shihao.instrumentedunittests;
 
 import android.content.Intent;
-import android.support.test.espresso.web.action.AtomAction;
 import android.support.test.espresso.web.assertion.WebViewAssertions;
 import android.support.test.espresso.web.sugar.Web;
 import android.support.test.espresso.web.webdriver.DriverAtoms;
@@ -16,10 +15,6 @@ import org.junit.runner.RunWith;
 
 import me.shihao.espressotests.WebViewActivity;
 
-import static android.support.test.espresso.web.sugar.Web.onWebView;
-import static android.support.test.espresso.web.webdriver.DriverAtoms.findElement;
-import static android.support.test.espresso.web.webdriver.DriverAtoms.webClick;
-import static org.hamcrest.Matchers.containsString;
 
 /**
  * Created by shihao on 2017/3/3.
@@ -37,15 +32,15 @@ public class WebViewActivityTest {
         intent.putExtra(WebViewActivity.EXTRA_URL, "http://m.baidu.com");
         activityTestRule.launchActivity(intent);
 
-        //通过name为"word"找到搜索输入框
-        onWebView().withElement(findElement(Locator.NAME, "word"))
+//        //通过name为"word"找到搜索输入框
+        Web.onWebView().withElement(DriverAtoms.findElement(Locator.NAME, "word"))
                 //往输入框中输入字符串"android"
                 .perform(DriverAtoms.webKeys("android"))
                 //通过id为"index-bn"找到"百度一下"按钮
                 .withElement(DriverAtoms.findElement(Locator.ID, "index-bn"))
                 //执行点击事件
-                .perform(webClick())
-                //通过id为"results"找到结果div
+                .perform(DriverAtoms.webClick())
+                //通过id为"results"找到结果div  在Chrome调试中Console中输入 `$('#results')` 即可 查找d为"results"的标签
                 .withElement(DriverAtoms.findElement(Locator.ID, "results"))
                 //检查div中是否包含字符串"android"
                 .check(WebViewAssertions.webMatches(DriverAtoms.getText(), Matchers.containsString("android")));
